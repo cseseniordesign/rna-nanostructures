4c6240e549b622e631a5ae4fe4704552506eecff
Josh Bearden <josh.bearden@huskers.unl.edu>
2022-02-17 17:27:46 -0600

    Reformatted text, still trying to get image to appear

--- hcc_rnamake_portal/frontend/src/pages/FAQ.jsx
+++ hcc_rnamake_portal/frontend/src/pages/FAQ.jsx
@@ -1,21 +1,24 @@
 import React from "react";
-import basepair from "../images/base-pair.png";
+import basepair from "../images/basepair.png";
 
 function FAQ() {
     return (
-        <div className="faq">
-            <div className="container">
-                <div className="col-lg-8">
-                    <p>
-                        <a className="base-pair">
-                            <image src={basepair} />
-                        </a>
-                    </p>
-                </div>
-                <div>
-                    <p>
-                        Sample text
-                    </p>
+        <div>
+            <div className="faq">
+                <div className="container">
+                    <div className="text" align='left'>
+                        RNAMake leverages an extensive motif library and pathfinding algorithms to automate the assembly of RNA 3D motifs in design.
+                        Specifically, RNAMake is capable of generating RNA segments in 3D composed of continuous chains of RNA motifs that that twist and translate between any two desired helical endpoints.
+                        Submission requires a PDB containing RNA that has at least 2 basepair ends.
+                        An example structure is shown in the image. This name tells the webserver the start and end of a submitted structure.
+                        <br /><br />
+                        The basepair name represents the basepair's two contained residues.
+                        The basepair name in the format chain is appended to its residue number.
+                        In the case of A141-A162, it means that there is a basepair between residue 141 on chain A to residue 161 also on chain A. 
+                    </div>
+                    <div className="image">
+                        <image src={basepair} alt='Image of Base Pair' />
+                    </div>
                 </div>
             </div>
         </div>
