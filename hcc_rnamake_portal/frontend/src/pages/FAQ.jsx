cb72d5199d0998d6e26289414ca82bbd2072fdb8
Josh Bearden <josh.bearden@huskers.unl.edu>
2022-02-18 16:33:56 -0600

    Added grid styling; still can't get image to appear

--- hcc_rnamake_portal/frontend/src/pages/FAQ.jsx
+++ hcc_rnamake_portal/frontend/src/pages/FAQ.jsx
@@ -1,26 +1,25 @@
 import React from "react";
+import Grid from '@mui/material/Grid';
 import basepair from "../images/basepair.png";
 
 function FAQ() {
     return (
-        <div>
-            <div className="faq">
-                <div className="container">
-                    <div className="text" align='left'>
-                        RNAMake leverages an extensive motif library and pathfinding algorithms to automate the assembly of RNA 3D motifs in design.
-                        Specifically, RNAMake is capable of generating RNA segments in 3D composed of continuous chains of RNA motifs that that twist and translate between any two desired helical endpoints.
-                        Submission requires a PDB containing RNA that has at least 2 basepair ends.
-                        An example structure is shown in the image. This name tells the webserver the start and end of a submitted structure.
-                        <br /><br />
-                        The basepair name represents the basepair's two contained residues.
-                        The basepair name in the format chain is appended to its residue number.
-                        In the case of A141-A162, it means that there is a basepair between residue 141 on chain A to residue 161 also on chain A. 
-                    </div>
-                    <div className="image">
-                        <image src={basepair} alt='Image of Base Pair' />
-                    </div>
-                </div>
-            </div>
+        <div className='faq'>
+            <Grid container spacing={2}>
+                <Grid item lg={6}>
+                    RNAMake leverages an extensive motif library and pathfinding algorithms to automate the assembly of RNA 3D motifs in design.
+                    Specifically, RNAMake is capable of generating RNA segments in 3D composed of continuous chains of RNA motifs that that twist and translate between any two desired helical endpoints.
+                    Submission requires a PDB containing RNA that has at least 2 basepair ends.
+                    An example structure is shown in the image. This name tells the webserver the start and end of a submitted structure.
+                    <br /><br />
+                    The basepair name represents the basepair's two contained residues.
+                    The basepair name in the format chain is appended to its residue number.
+                    In the case of A141-A162, it means that there is a basepair between residue 141 on chain A to residue 161 also on chain A. 
+                </Grid>
+                <Grid item lg={6}>
+                    <image src={basepair} alt='Image of Base Pair' />
+                </Grid>
+            </Grid>
         </div>
     );
 }
