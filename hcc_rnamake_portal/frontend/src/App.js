3b2eb67a2b674b6835efb16832e7812b1dc52226
ahamad2 <59970176+ahamad2@users.noreply.github.com>
2022-02-24 15:23:39 -0600

    Feature/refactor react routing and element scaling(#107)(#100)
    * changed react routing
    * fixed element scaling inside react app when its a plugin

--- hcc_rnamake_portal/frontend/src/App.js
+++ hcc_rnamake_portal/frontend/src/App.js
@@ -18,11 +18,11 @@ function App() {
           <Router>
               <Navigation />
               <Switch>
-                  <Route path="/" exact component={() => <Workspace />} />
-                  <Route path="/past-experiments" exact component={() => <PastExperiments />} />
-                  <Route path="/file-upload" exact component={() => <FileUpload />} />
-                  <Route path="/new-experiment" exact component={() => <NewExperiment />} />
-                  <Route path="/FAQ" exact component={() => <FAQ />} />
+                  <Route path="/rnamake_portal/workspace" exact component={() => <Workspace />} />
+                  <Route path="/rnamake_portal/past-experiments" exact component={() => <PastExperiments />} />
+                  <Route path="/rnamake_portal/file-upload" exact component={() => <FileUpload />} />
+                  <Route path="/rnamake_portal/new-experiment" exact component={() => <NewExperiment />} />
+                  <Route path="/rnamake_portal/FAQ" exact component={() => <FAQ />} />
               </Switch>
           </Router>
           {/* <header className="App-header">
