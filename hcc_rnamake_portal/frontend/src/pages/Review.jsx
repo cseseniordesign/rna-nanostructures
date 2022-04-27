6be36b4e64616513b8ed69c3c109f80baf5716db
unknown <katie.gerot@huskers.unl.edu>
2022-04-26 21:55:51 -0500

    Review Documentation

--- hcc_rnamake_portal/frontend/src/pages/Review.jsx
+++ hcc_rnamake_portal/frontend/src/pages/Review.jsx
@@ -1,14 +1,15 @@
 import * as React from 'react';
 import Typography from '@mui/material/Typography';
-//import List from '@mui/material/List';
-//import ListItem from '@mui/material/ListItem';
-//import ListItemText from '@mui/material/ListItemText';
 import Grid from '@mui/material/Grid';
 
-
-
+/**
+ * Review()
+ * @param {*} props 
+ * @returns Review of Submission
+ */
 export default function Review(props) {
   
+  // Information for left column displayed at review
   const col1 = [
     { name: 'Experiment Name', detail: props.settings.name },
     { name: 'Description', detail: props.settings.description },
@@ -17,6 +18,7 @@ export default function Review(props) {
     { name: 'End Base Pair', detail: props.settings.endingBase},
   ];
   
+  // Information for right column displayed at review
   const col2 = [
     { name: 'Number of Designs', detail: props.settings.designs },
     { name: 'Number of Sequences', detail: props.settings.scaffolds },
@@ -32,6 +34,7 @@ export default function Review(props) {
       <Grid container spacing={2}>
         <Grid item container direction="column" xs={12} sm={6}>
         <Grid container>
+            {/* Dynamically generates name and details of each item to review in the left column*/}
             {col1.map((col1) => (
               <React.Fragment key={col1.name}>
                 <Grid item xs={6}>
@@ -46,6 +49,7 @@ export default function Review(props) {
         </Grid>
         <Grid item container direction="column" xs={12} sm={6}>
           <Grid container>
+            {/* Dynamically generates name and details of each item to review in the right column*/}
             {col2.map((col2) => (
               <React.Fragment key={col2.name}>
                 <Grid item xs={6}>
